b8f1c9acf7ca5961f05829f8992a49d2d2a76bcf
aidyn47 <75780969+aidyn47@users.noreply.github.com>
2021-04-21 14:38:54 +0600

    Add files via upload

---
+++ test1.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+				website
+        </title>
+		<meta charset="utf-8">
+
+            <style> 
+				.fig {
+					display: block;
+					margin-top: 10px;
+					text-align: right;
+					font-style: normal;
+					margin-bottom: 0px;
+
+				}
+					div {
+						text-align: left;
+						margin-top: 150px;
+						font-size: 40px;
+						text-indent: 10%;
+
+					}
+					
+					body {
+						background: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(http://bgfons.com/upload/ornaments_texture1154.jpg);
+						color: #fff;
+						font-family: sans-serif;
+						background-size: cover;
+						}
+					p {
+						margin-top: -1%;
+					}
+            </style>
+    </head>
+    <body>
+		<p><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRO78WMhKfGfS58jZuKg-tU5d6yr6NHjiMgDA&usqp=CAU" width="450"align="right" hspace="22%" border="4">
+		</p>
+		
+		<div>
+		<h2>Aidyn</h2>
+		<h2>Mametov</h2>
+		<h2>T-SCA-19B</h2>
+		<h2>30.08.2003</h2>
+		</div>
+		<div>
+			<p>email: mametovaidyn2003@gmail.com</p>
+			<p>phone number: +996709622093</p>
+			
+		</div>
+		<figure class = "fig">
+			<a href="https://youtu.be/dQw4w9WgXcQ">More</a>
+		</figure>
+    </body>
+</html>
